8d1ab6eb1f0658970b6e384fd57e14fc05757106
Eitan <ekassuto@gmail.com>
2025-02-18 22:18:53 +0200

    combined dinov2 with RIDE

--- model/model.py
+++ model/model.py
@@ -1,3 +1,4 @@
+from model.dinov2 import Expert_vision_transformer, vision_transformer
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -147,3 +148,11 @@ class ResNeXt152Model(Model):
             self.backbone = ResNeXt.ResNext(ResNeXt.Bottleneck, [3, 8, 36, 3], groups=32, width_per_group=4, dropout=None, num_classes=num_classes, reduce_dimension=reduce_dimension, layer3_output_dim=layer3_output_dim, layer4_output_dim=layer4_output_dim)
         else:
             self.backbone = Expert_ResNeXt.ResNext(Expert_ResNeXt.Bottleneck, [3, 8, 36, 3], groups=32, width_per_group=4, dropout=None, num_classes=num_classes, reduce_dimension=reduce_dimension, layer3_output_dim=layer3_output_dim, layer4_output_dim=layer4_output_dim, num_experts=num_experts)
+
+class DINOv2Model(Model):
+    def __init__(self, num_classes,  patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, num_experts=3, returns_feat=True, expert_start_layer=10, pretrained_path=None):
+        super().__init__(num_classes, patch_size, embed_dim, depth, num_heads, mlp_ratio, num_experts, pretrained_path)
+        if num_experts == 1:
+            self.backbone = vision_transformer.DinoVisionTransformer(patch_size, embed_dim, depth, num_heads, mlp_ratio, num_classes, pretrained_path)
+        else:
+            self.backbone = Expert_vision_transformer.RIDEDinoVisionTransformer(patch_size=patch_size, embed_dim=embed_dim, depth=depth, num_heads=num_heads, mlp_ratio=mlp_ratio, num_experts=num_experts, num_classes=num_classes, returns_feat=returns_feat, expert_start_layer=expert_start_layer, pretrained_path=pretrained_path)
